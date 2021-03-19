42f09d3b1ff16bb8e5e19b00a69d8b7ff252cb18
Innuendo416 <collinstone2291@icloud.com>
2021-03-19 15:08:00 -0400

    Banner animations

--- script.js
+++ script.js
@@ -4,11 +4,29 @@ const handleBanner = (e) => {
 		e.target.removeEventListener('click', closeBanner)
 		e.target.addEventListener('click', handleBanner)
 		setTimeout(() => {
-			gsap.set(e.target, {clearProps: 'all'})
-		},500)
+			gsap.set('.active, .inactive', {clearProps: 'all'})
+			Array.from(document.querySelectorAll('div')).forEach(ele => {
+				if(e.target === ele){
+					ele.classList.toggle('active')
+				} else {
+					ele.classList.toggle('inactive')
+				}			
+			})
+		},600)
+
 	}
+	Array.from(document.querySelectorAll('div')).forEach( ele => {
+		if(e.target === ele){
+			ele.classList.toggle('active')
+		} else {
+			ele.classList.toggle('inactive')
+		}
+	})
 	const tl = gsap.timeline()
-	tl.to(e.target, {height: '100%', width: '100%', left: '0', top: '0', zIndex: 2, duration: .5})
+	const activeBefore = CSSRulePlugin.getRule('.active::before')
+	tl.to('.inactive', {height: '0%', opacity: 0, top: '+50%', duration: .75, ease: Circ.easeIn})
+	tl.to('.active', {height: '100%', width: '100%', left: '0', top: '0', zIndex: 3 ,duration: .25, ease: Circ.easeIn}, '-=.25')
+	tl.fromTo(activeBefore , {opacity: 0}, {opacity: '.7', duration: .5, ease: Circ.easeIn}, '-=.5')
 	e.target.addEventListener('click', closeBanner)
 	e.target.removeEventListener('click', handleBanner)
 }
